2617a3f1aa0be3747160e7983b187669b7563800
user <user@uluru280.local>
2016-01-13 16:06:08 +0900

    Initial Commit

---
+++ MyBook/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyBook
+//
+//  Created by user on 2016/01/13.
+//  Copyright © 2016年 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyBook/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyBook
+//
+//  Created by user on 2016/01/13.
+//  Copyright © 2016年 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
